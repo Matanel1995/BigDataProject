097848e2da14dbf732e1331d54484101a804cd9f
Matanel1995 <matanel5555@gmail.com>
2023-06-01 13:57:12 +0300

    add react project

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='main'>
+      <h1>TEST YA MANIAK</h1>
+      <div className='test'>
+        <h1>shahor</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
