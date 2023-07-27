cf56a0eda9f6bf8d3d79843fb0914419608a5283
Matanel1995 <matanel5555@gmail.com>
2023-07-27 18:56:38 +0300

    added search page functunallity

--- Simulator/index.js
+++ Simulator/index.js
@@ -75,3 +75,7 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+const telescopeKeys = Object.keys(telescopeData);
+console.log(telescopeKeys)
