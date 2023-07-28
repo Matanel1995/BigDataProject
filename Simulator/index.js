0f8711504cd2f5ca1be245a042965551d0ba4605
adiel1892 <adiel1892@gmail.com>
2023-07-28 13:31:08 +0300

    adding sun

--- Simulator/index.js
+++ Simulator/index.js
@@ -71,7 +71,7 @@ async function generateSimulatedData() {
 }
 
 // Start the server
-const port = 3000;
+const port = 2000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
--- WebScrapping/sun.js
+++ WebScrapping/sun.js
@@ -14,7 +14,6 @@ async function scrapeSunInfo() {
     const $ = cheerio.load(response.data);
 
     const sunData = [];
-
     const dataRows = $('.objectdata tbody tr');
 
     dataRows.each((index, element) => {
@@ -51,6 +50,49 @@ app.get('/getSunData', async (req, res) => {
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'Error fetching Sun data' });
+  }
+});
+
+async function scrapeClosestApproach() {
+  try {
+    const url = 'https://theskylive.com/sun-info';
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+
+    const closestApproachData = {};
+
+    // Extract the description
+    const descriptionElement = $('h1:contains("Closest Approach of The Sun to Earth")').next('p');
+    const description = descriptionElement.text().trim();
+
+    // closestApproachData['Title'] = 'Closest Approach of The Sun to Earth';
+    closestApproachData['Description'] = description;
+
+    $('.keyinfobox').each((index, element) => {
+      const label = $(element).find('label').text().trim();
+      const value = $(element).find('ar').text().trim();
+
+      if (label === 'Date' || label === 'Distance Kilometers' || label === 'Distance AU') {
+        closestApproachData[label] = value;
+      }
+    });
+
+    return closestApproachData;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+}
+
+
+app.get('/getClosestApproach', async (req, res) => {
+  try {
+      const closestApproachData = await scrapeClosestApproach();
+
+      res.send({ closestApproachData });
+  } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'Error fetching Sun data' });
   }
 });
 
